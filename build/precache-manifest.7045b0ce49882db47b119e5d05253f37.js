920614094c98078fdd09733b98b41bda880b2770
Aldo Bernardes <aldo-maciel@hotmail.com>
2020-05-02 18:08:51 -0300

    build:  add build folder

---
+++ build/precache-manifest.7045b0ce49882db47b119e5d05253f37.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93961228671c5c8510e1a508350d0e0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "151ac3b29c6124ae6a93",
+    "url": "/static/css/main.747ecb59.chunk.css"
+  },
+  {
+    "revision": "7f81cf2a6869cb8f7ad8",
+    "url": "/static/js/2.106508b3.chunk.js"
+  },
+  {
+    "revision": "b1c48fb50b52ff7e51ea57a4c76b798d",
+    "url": "/static/js/2.106508b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151ac3b29c6124ae6a93",
+    "url": "/static/js/main.7f7afd7b.chunk.js"
+  },
+  {
+    "revision": "d82d9c57ecaaa9eac3f2",
+    "url": "/static/js/runtime-main.1efafa64.js"
+  },
+  {
+    "revision": "a5ee087912cbedb6022426ea0b41f8bd",
+    "url": "/static/media/Eva-Icons.a5ee0879.woff"
+  },
+  {
+    "revision": "a71dbd49ad4a52020638190e30ac52e0",
+    "url": "/static/media/Eva-Icons.a71dbd49.ttf"
+  },
+  {
+    "revision": "cae252678f70ff0200acde68a6e74640",
+    "url": "/static/media/Eva-Icons.cae25267.woff2"
+  },
+  {
+    "revision": "f0cc7d7de5048ae38b06b399bb2f0431",
+    "url": "/static/media/Eva-Icons.f0cc7d7d.eot"
+  },
+  {
+    "revision": "fe9c0c7fafa4c415df88cdd12e6b01b3",
+    "url": "/static/media/Eva-Icons.fe9c0c7f.svg"
+  }
+]);
