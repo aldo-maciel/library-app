e43e4ef4b17a611dc3bf2d720fc79e2a9c0aff9c
Aldo Bernardes <aldo-maciel@hotmail.com>
2020-05-02 18:05:21 -0300

    feat: add history routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createHashHistory } from 'history';
 import moment from 'moment';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Router, Switch } from 'react-router-dom';
+
 import * as serviceWorker from './service-worker';
 import './index.scss';
 import 'moment/locale/pt-br';
@@ -13,16 +15,16 @@ import { LoginRender } from './components/login/login.render';
 moment.locale('pt-br');
 
 ReactDOM.render(
-    <BrowserRouter>
+    <Router history={ createHashHistory() }>
         <Switch>
             <Route path="/" exact component={ LoginRender }/>
             <Route path="/home" component={ HomePageRender }/>
             <Route path="/livros/cadastrar" component={ BookRegisterRender }/>
             <Route path="/livros/:id" component={ BookRegisterRender }/>
             <Route path="/livros" component={ BookRender }/>
-            <Route path="*" component={() => <h1 className="w100 min-vh-100 d-flex justify-content-center align-items-center">Page not found</h1>} />
+            <Route path="*" component={ () => <h1 className="w100 min-vh-100 d-flex justify-content-center align-items-center">Page not found</h1> }/>
         </Switch>
-    </ BrowserRouter>, document.getElementById('root'));
+    </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
