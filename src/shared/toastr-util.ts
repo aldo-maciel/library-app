1b36f5191c8525556c46a8b446b9edb4fd450ec9
Aldo Bernardes <aldo-maciel@hotmail.com>
2020-05-02 18:06:50 -0300

    feat: improve error

--- src/shared/toastr-util.ts
+++ src/shared/toastr-util.ts
@@ -3,7 +3,7 @@ import { dictionary } from './dictionary';
 
 export const onSuccess = () => toastr.success(dictionary.SUCCESS_MESSAGE, dictionary.SUCCESS_TITLE);
 
-export const onError = (error: Error) => {
+export const onError = (error: Error, message = '') => {
     console.error(error);
-    toastr.error(error.message || dictionary.ERROR_MESSAGE, dictionary.ERROR_TITLE);
+    toastr.error(message || error.message || dictionary.ERROR_MESSAGE, dictionary.ERROR_TITLE);
 };
